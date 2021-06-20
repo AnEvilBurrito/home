2cebc0d46a40417f3b4ac4785fe50be8f733f243
AnEvilBurrito <47137689+AnEvilBurrito@users.noreply.github.com>
2021-06-20 19:38:23 +1000

    hello from PC

--- ts-built/engine/World.js
+++ ts-built/engine/World.js
@@ -1,10 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VirtualPoint = void 0;
-class VirtualPoint {
+export class VirtualPoint {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
-exports.VirtualPoint = VirtualPoint;
